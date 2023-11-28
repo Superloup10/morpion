70ec31fe21a9c68d19b66d41ed0b80e430e198ec
Superloup10 <Superloup10@gmail.com>
2023-11-28 12:10:26 +0100

    Add medium mode

--- src/fr/wolfdev/cda/morpion/Morpion.java
+++ src/fr/wolfdev/cda/morpion/Morpion.java
@@ -63,6 +63,24 @@ public class Morpion {
                 break;
             }
             case "moyen": { // Ordinateur défensif
+                // Si l'utilisateur a l'un des quatre coins, mais pas les autres, on prend la case du centre si elle est vide
+                if(grid[1][1] == ' ' && ((grid[0][0] == 'X') ^ (grid[0][2] == 'X') ^ (grid[2][0] == 'X') ^ (grid[2][2] == 'X'))) {
+                    fillGridWithUserTwoValue(grid, "11");
+                }
+                else {
+                    if(blockLineOrColumnOrDiagonal(grid)) {
+                        // L'ordinateur a joué, aucune action supplémentaire nécessaire
+                    }
+                    else if(grid[1][1] == ' ') {
+                        // La case centrale est disponible, jouer au centre
+                        fillGridWithUserTwoValue(grid, "11");
+                    }
+                    else {
+                        // Si aucune action spécifique n'est prise, jouer aléatoirement
+                        randomComputerHit(grid);
+                    }
+                }
+
                 break;
             }
             case "difficile": {
@@ -86,6 +104,49 @@ public class Morpion {
         }
         while(!isComputerHit);
     }
+
+    private static boolean blockLineOrColumnOrDiagonal(char[][] grid) {
+        for(int i = 0; i < 3; i++) {
+            if(blockIfNecessary(grid, i, 0, i, 1, i, 2)) {
+                return true;
+            }
+        }
+
+        for(int i = 0; i < 3; i++) {
+            if(blockIfNecessary(grid, 0, i, 1, i, 2, i)) {
+                return true;
+            }
+        }
+
+        if(blockIfNecessary(grid, 0, 0, 1, 1, 2, 2)) {
+            return true;
+        }
+
+        if(blockIfNecessary(grid, 0, 2, 1, 1, 2, 0)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private static boolean blockIfNecessary(char[][] grid, int line1, int column1, int line2, int column2, int line3, int column3) {
+        if(grid[line1][column1] == ' ' && grid[line2][column2] == 'X' && grid[line3][column3] == 'X') {
+            grid[line1][column1] = 'O';
+            return true;
+        }
+        else if(grid[line2][column2] == ' ' && grid[line1][column1] == 'X' && grid[line3][column3] == 'X') {
+            grid[line2][column2] = 'O';
+            return true;
+        }
+        else if(grid[line3][column3] == ' ' && grid[line1][column1] == 'X' && grid[line2][column2] == 'X') {
+            grid[line3][column3] = 'O';
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
     public static boolean isWinner(char[][] grid, char player) {
         for(int i = 0; i < grid.length; i++) {
             if((grid[i][0] == player && grid[i][1] == player && grid[i][2] == player) ||
